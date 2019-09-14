4f5c602084265ec0873cb9d7f90e8a1721e812b2
Henrik Olsson <hnrklssn@gmail.com>
2019-09-14 22:52:27 +0000

    add schema comments

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { GraphQLObjectType, GraphQLString } from "graphql";
 import { v1 as neo4j } from "neo4j-driver";
 import { makeAugmentedSchema } from "neo4j-graphql-js";
 
+// First element of each typedef has to be a scalar value, it cannot be an object
+// due to makeAugmentedSchema bug!
 const typeDefs = `
+"""A type of items that can be contained in a container."""
 interface UnitType {
+    """Unique human friendly identifier"""
     name: String!
     barcode: Identifier
+    # Probably want non-null Identifier! in the future and force assigning
+    # a barcode on construction, but it requires some manual schema definitions
+    # to create custom Mutations instead of the handy auto-generated ones
 }
+"""An item that can be contained in a container"""
 interface Unit {
     container: Container
 }
@@ -17,6 +25,7 @@ enum BarcodeType {
     EAN
 }
 
+"""Singleton type which also is able to contain other items. Containers can be indefinitely nested and could represent things such as a room, a shelf, a toolbox, a fridge etc."""
 type Container implements Unit & UnitType {
     name: String!
     barcode: Identifier @relation(name: "HAS_BARCODE", direction: "OUT")
@@ -24,24 +33,31 @@ type Container implements Unit & UnitType {
     containsItems: [Unit!] @relation(name: "POSITION", direction: "IN")
     containsStacks: [ProductStack!]
 }
+
+"""Singleton type for tying a specific item to specific barcode and container"""
 type Item implements Unit & UnitType {
     name: String!
     barcode: Identifier @relation(name: "HAS_BARCODE", direction: "OUT")
     container: Container @relation(name: "POSITION", direction: "OUT")
 }
+"""Relation that represents a number of a type of item being located in a certain container"""
 type ProductStack @relation(name: "CONTAINS_STACK") {
     amount: Int!
     to: Container!
     from: ProductType!
 }
+"""Class of product of which there can be more than one of the same item, all equivalent and with the same barcode. The items do not have to be all in the same container at once, but can be separated into ProductStacks."""
 type ProductType implements UnitType {
     name: String!
     barcode: Identifier @relation(name: "HAS_BARCODE", direction: "OUT")
     stacks: [ProductStack!]
 }
+"""A barcode that can be of any type, e.g. QR or EAN codes"""
 type Identifier {
+    """The value encoded in the barcode"""
     code: String!
     barcodeType: BarcodeType!
+    """The category of items represented by this barcode"""
     unitType: UnitType @relation(name: "HAS_BARCODE", direction: "IN")
 }
 `;
