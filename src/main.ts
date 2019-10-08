56d66caadf2568f68ba8d95857d2ad344ddc73bb
Henrik Olsson <hnrklssn@gmail.com>
2019-10-08 10:38:20 +0000

    closes #2 - add convenience mutations for adding item and barcode atomically, transitive container lists

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from "apollo-server";
 import { GraphQLObjectType, GraphQLString } from "graphql";
 import { v1 as neo4j } from "neo4j-driver";
-import { makeAugmentedSchema } from "neo4j-graphql-js";
+import { makeAugmentedSchema, neo4jgraphql } from "neo4j-graphql-js";
 
 // First element of each typedef has to be a scalar value, it cannot be an object
 // due to makeAugmentedSchema bug!
@@ -15,9 +15,16 @@ interface UnitType {
     # a barcode on construction, but it requires some manual schema definitions
     # to create custom Mutations instead of the handy auto-generated ones
 }
+
 """An item that can be contained in a container"""
 interface Unit {
     container: Container
+    """List a unit's container and the container's container and so on,
+    with the nearest container first. Limited to 20 containers."""
+    # TODO: It would be nice to only have to implement this once,
+    # but the current neo4j-graphql-js implementation does not support interface
+    # labels and thus implementations
+    transitiveContainers: [Container!]!
 }
 
 enum BarcodeType {
@@ -25,13 +32,33 @@ enum BarcodeType {
     EAN
 }
 
-"""Singleton type which also is able to contain other items. Containers can be indefinitely nested and could represent things such as a room, a shelf, a toolbox, a fridge etc."""
+"""Singleton type which also is able to contain other items.
+Containers can be indefinitely nested and could represent things such as a room,
+a shelf, a toolbox, a fridge etc."""
 type Container implements Unit & UnitType {
     name: String!
     barcode: Identifier @relation(name: "HAS_BARCODE", direction: "OUT")
     container: Container @relation(name: "POSITION", direction: "OUT")
-    containsItems: [Unit!] @relation(name: "POSITION", direction: "IN")
+    containsUnits: [Unit!] @relation(name: "POSITION", direction: "IN")
     containsStacks: [ProductStack!]
+
+    """List a unit's container and the container's container and so on,
+    with the nearest container first. Limited to 20 containers."""
+    transitiveContainers: [Container!]!
+    # Limited to 20 to avoid exploding query if graph is malformed
+    @cypher(statement: """
+        MATCH p = (this)-[:POSITION*..20]->(c :Container)
+        WHERE NOT (c)-[:POSITION]->()
+        UNWIND tail(nodes(p)) as n
+        RETURN n;
+    """)
+
+    """List this container's subcontainers and their subcontainers and so on. Selfincluded."""
+    transitiveSubcontainers: [Container!]!
+    @cypher(statement: """
+        MATCH (this)<-[:POSITION *0..20]-(c :Container)
+        RETURN c;
+    """)
 }
 
 """Singleton type for tying a specific item to specific barcode and container"""
@@ -39,19 +66,38 @@ type Item implements Unit & UnitType {
     name: String!
     barcode: Identifier @relation(name: "HAS_BARCODE", direction: "OUT")
     container: Container @relation(name: "POSITION", direction: "OUT")
+    """List a unit's container and the container's container and so on,
+    with the nearest container first. Limited to 20 containers."""
+    # Limited to 20 to avoid exploding query if graph is malformed
+    transitiveContainers: [Container!]!
+    @cypher(statement: """
+        MATCH p = (this)-[:POSITION*..20]->(c :Container)
+        WHERE NOT (c)-[:POSITION]->()
+        UNWIND tail(nodes(p)) as n
+        RETURN n;
+    """)
 }
+
 """Relation that represents a number of a type of item being located in a certain container"""
 type ProductStack @relation(name: "CONTAINS_STACK") {
+    "How many items the stack contains"
     amount: Int!
+    "Where the stack is"
     to: Container!
+    "What type of items the stack contains"
     from: ProductType!
 }
-"""Class of product of which there can be more than one of the same item, all equivalent and with the same barcode. The items do not have to be all in the same container at once, but can be separated into ProductStacks."""
+
+"""Class of product of which there can be more than one of the same item,
+all equivalent and with the same barcode.
+The items do not have to be all in the same container at once,
+but can be separated into ProductStacks."""
 type ProductType implements UnitType {
     name: String!
     barcode: Identifier @relation(name: "HAS_BARCODE", direction: "OUT")
-    stacks: [ProductStack!]
+    stacks: [ProductStack!]!
 }
+
 """A barcode that can be of any type, e.g. QR or EAN codes"""
 type Identifier {
     """The value encoded in the barcode"""
@@ -62,12 +108,42 @@ type Identifier {
 }
 
 type Mutation {
-#TODO: supress mutations we do not want accessed
-"""The proper way to add a new product type"""
+    #TODO: supress mutations we do not want accessed
+    """The proper way to add a new product type"""
     addProductType(name: String!, barcode: String!, barcodeType: BarcodeType!): ProductType
     @cypher(statement: """
-        CREATE (pt: ProductType {name: $name})-[:HAS_BARCODE]->(b: Identifier {code: $barcode, barcodeType: $barcodeType})
+        CREATE (pt: ProductType {name: $name})-[:HAS_BARCODE]->
+        (b: Identifier {code: $barcode, barcodeType: $barcodeType})
         RETURN pt""")
+
+    """The proper way to add a new singleton item"""
+    addItem(name: String!, barcode: String!, barcodeType: BarcodeType!): Item
+    @cypher(statement: """
+        CREATE (i: Item {name: $name})-[:HAS_BARCODE]->(b: Identifier {code: $barcode, barcodeType: $barcodeType})
+        RETURN i""")
+
+    """Move stack items from one container to another. If the first stack does not
+    have $amount of items, the call will fail."""
+    moveStackItems(
+        "barcode of product type"
+        product: String!,
+        "barcode of current container"
+        from: String!,
+        "barcode of container to move items to"
+        to: String!,
+        "number of product items to move"
+        amount: Int!): Boolean!
+    @cypher(statement: """
+        MATCH (pt: ProductType)-[:HAS_BARCODE]->(prod_code: Identifier {code: $product}),
+            (pt)-[from_stack: CONTAINS_STACK]->(:Container)-[:HAS_BARCODE]->(from_code: Identifier {code: $from}),
+            (c2: Container)-[:HAS_BARCODE]->(to_code: Identifier {code: $to})
+        WHERE from_stack.amount >= $amount
+        MERGE (pt)-[to_stack: CONTAINS_STACK]->(c2)
+            ON CREATE SET to_stack.amount = $amount
+            ON MATCH SET to_stack.amount = coalesce(to_stack.amount, 0) + $amount
+        SET from_stack.amount = from_stack.amount - $amount
+        RETURN true;
+    """)
 }
 `;
 
@@ -78,7 +154,7 @@ const driver = neo4j.driver(
 
 const schema = makeAugmentedSchema({ typeDefs });
 
-const server = new ApolloServer({ schema, context: { driver } });
+const server = new ApolloServer({ schema, context: { driver }, });
 
 server.listen(8082, "0.0.0.0").then(({ url }) => {
       console.log(`GraphQL API ready at ${url}`);
